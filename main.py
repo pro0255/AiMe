6fffed850d2c6c4d567640fa942a3d286730e3ed
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-09-16 15:42:08 +0200

    feat: first prompt sent to openai api

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 
 
 from dotenv import dotenv_values
+from langchain.prompts import PromptTemplate
+
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+
+
+
+
 
 config = dict(dotenv_values(".env"))
 
-print(config)
+
+prompt = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
+print(prompt.format(product="colorful socks"))
+
+
+model = config["MODEL"]
+api_key = config['OPENAI_API_KEY']
+chat = ChatOpenAI(model_name=model, openai_api_key=api_key)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant that translates English to French."),
+    HumanMessage(content="I love programming.")
+]
+
+
+result = chat(messages)
+
+print(result)
